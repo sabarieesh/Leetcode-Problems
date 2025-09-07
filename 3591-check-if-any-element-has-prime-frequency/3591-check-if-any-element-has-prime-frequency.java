20ff70e762d8aed5708c3865402f2a5b8c7bd7f1
Sabarieesh M <sabarisabarieesh@gmail.com>
2025-09-07 11:41:28 +0530

    Time: 12 ms (5.13%), Space: 44.6 MB (5.71%) - LeetHub

---
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkPrimeFrequency(int[] nums) {
+       ArrayList<Integer> list=new ArrayList<>();
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int i=0;i<nums.length;i++) {
+  map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+       }
+for(int i=0;i<nums.length;i++){
+    if(!list.contains(nums[i]))
+    list.add(nums[i]);
+}
+
+ ArrayList<Integer> ans=new ArrayList<>();
+
+for(int i:list){
+
+
+    int a=map.get(i);
+    if(a==2)
+    ans.add(i);
+    int count=0;
+    for(int j=2;j<=a;j++){
+        if(a%j==0)
+        count++;
+    }
+    if(count==1)
+    ans.add(i);
+}
+System.out.print(ans);
+System.out.print(map);
+
+
+if(ans.size()>0)
+return true;
+       return false;
+    }
+}
